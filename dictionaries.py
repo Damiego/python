7c715a58fff0653cde052208eef5b6061867941f
Simeon-Lancelot Damiego David <117737921+Damiego@users.noreply.github.com>
2023-04-05 10:51:25 +0100

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#dictionaries 
+#using matric no to identify a person and he/she score in an exam 
+Namesandmatricno = {
+    "20/0738" :  "dami " "50",
+    "20/0421" : "jane " "40",
+    "19/0742" : "james " "20",   
+}
+
+print(Namesandmatricno['19/0742'])
