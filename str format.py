34a4cb6a6618da56fa180adfbcf7de3f30e2c9b8
Simeon-Lancelot Damiego David <117737921+Damiego@users.noreply.github.com>
2023-04-26 14:55:19 +0100

    Create str format.py

---
+++ str format.py
@@ -0,0 +1,5 @@
+#str format
+name =  "dami"
+matric_no =  "20/34"
+print("This {} he has a matric no {} " .format(name,matric_no))
+#optional method that gives users more control when displaying the output 
