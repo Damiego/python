1a0895fd0b40a4b63a5d03aaaf20ef26299f1cec
Simeon-Lancelot Damiego David <117737921+Damiego@users.noreply.github.com>
2023-04-26 16:14:46 +0100

    Update reading files in.py

--- reading files in.py
+++ reading files in.py
@@ -1,3 +1,4 @@
 #reading files in py
 with open ('Students Email.txt') as file :
     print(file.read())
+#this is reading the file i created in my vs code, if you created your file in your pc you have to copy and paste the path where the ('Students Email.txt') is 
